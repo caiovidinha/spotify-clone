db92383bb8a52046441db15cab0335bed06df6f0
Caio Vidinha <caiovidinha@id.uff.br>
2023-12-28 17:33:43 -0300

    pre API

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,14 @@
+ /** 
+ * @license Apache-2.0
+ * @copyright Caio Vidinha 2023
+ */
+
+ 'use strict'
+
+ const querystring = require('querystring')
+
+ const apiConfig = require('../config/api.config')
+
+ const auth = (req, res) => {
+    const state = false
+ }
---
+++ src/utils/helpers.utils.js
@@ -0,0 +1,8 @@
+ /** 
+ * @license Apache-2.0
+ * @copyright Caio Vidinha 2023
+ */
+
+ 'use strict'
+
+ 
